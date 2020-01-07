5cceb2d4e2cb3b5c5f1beba9bb62bd9f9a6ddc75
tomas yaya <tomas.yaya@socialandbeyond.com>
2020-01-07 09:20:46 +0100

    add toggle functionallity to predictions list

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "./components/Input.jsx";
 import useAutocomplete from "./hooks/useAutocomplete";
 import options from "./data";
 
 const App = () => {
+  const [show, setShow] = useState(false);
   const [predictions, element, callback] = useAutocomplete(options);
   const handleChange = e => {
+    if (!show) {
+      setShow(true);
+    }
     callback(e.target.value);
   };
+  const handleClick = () => {
+    if (show) {
+      setShow(false);
+    }
+  };
+  const handleKeyPress = () => console.log("key press");
   return (
     <div>
       App
       <Input ref={element} onChange={handleChange} />
       <div>
-        {predictions.map((item, i) => (
-          <p key={i}>{item}</p>
-        ))}
+        {show &&
+          predictions.map((item, i) => (
+            <div
+              tabIndex={i}
+              role="button"
+              key={i}
+              onKeyPress={handleKeyPress}
+              onClick={handleClick}
+            >
+              <p>{item}</p>
+            </div>
+          ))}
       </div>
     </div>
   );
